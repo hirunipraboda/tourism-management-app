fee4121f4bb3dd31ef7c999c74656dba8cfb1db7
hettiarachchisithumini781 <hettiarachchisithumini781.com>
2026-09-24 22:20:19 +0530

    Create Tourist.cs

---
+++ backend/Models/Tourist.cs
@@ -0,0 +1,9 @@
+namespace tourism_management_app.Api.Models
+{
+    public class Tourist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}

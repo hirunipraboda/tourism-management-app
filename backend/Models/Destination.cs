8391da75aa9eade4b7e4d090e2a6dfc9b8635e91
hettiarachchisithumini781 <hettiarachchisithumini781.com>
2026-09-24 22:16:21 +0530

    Create Destination.cs

---
+++ backend/Models/Destination.cs
@@ -0,0 +1,8 @@
+namespace tourism_management_app.Api.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
